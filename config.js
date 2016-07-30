e029306150c3e46683583c6192829bbd5268adc4
Colin Aulds <caulds989@gmail.com>
2016-07-30 15:29:14 -0700

    hey again

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                       process.env.NODE_ENV === 'production' ?
+                       (process.env.NODE_ENV === 'production' ?
                             'mongodb://caulds989:Seoul989@ds031975.mlab.com:31975/mongo-shopping-list-aulds' :
-                            
+                            'mongodb://localhost/shopping-list-dev');
 exports.PORT = process.env.PORT || 8080;
